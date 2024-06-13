a8821424716d1e71c76899b9c5c5bb42e0cee9c1
Maitén Guadalupe Blanc <116910158+MaitenBlanc@users.noreply.github.com>
2024-06-13 12:00:23 -0300

    Delete 3.5 directory

--- 3.5/Ej_3.5.1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-ruta = "C:/Users/maite/OneDrive/Escritorio/Curso-NTT/Area_3/3.5/archivos/World_Development_Report_2021.csv"
-df = pd.read_csv(ruta, sep=',')
-
-df_tipo_datos = df.dtypes
-
-print(df_tipo_datos)
--- 3.5/Ej_3.5.11.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-ruta = "./archivos/indicadores.csv"
-
-df = pd.read_csv(ruta, sep=';')
-
-df.drop(columns=["Anno1996", "Anno2001"], inplace=True)
-
-print(df)
