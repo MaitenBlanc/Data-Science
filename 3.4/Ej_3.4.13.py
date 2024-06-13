7ea7a4866cac7997193c805dd6dd7ffa9efca0f4
Maitén Guadalupe Blanc <116910158+MaitenBlanc@users.noreply.github.com>
2024-06-13 12:00:15 -0300

    Delete 3.4 directory

--- 3.4/Ej_3.4.13.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('C:/Users/maite/OneDrive/Escritorio/Curso-NTT/Area_3/3.4/archivos/ejecucion-presupuestaria_Chile-febrero-2023.csv', sep=';')
-
-lista = ["IMPUESTOS","RENTAS DE LA PROPIEDAD","INGRESOS DE OPERACIÓN","VENTA DE ACTIVOS FINANCIEROS"]
-
-df_lista = df.loc[df["Nombre_Subtítulo"].isin(lista)]
-
-print(df_lista)
--- 3.4/Ej_3.4.3.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("C:/Users/maite/OneDrive/Escritorio/Curso-NTT/Area_3/3.4/archivos/Esculturas_Parque_Arqueolgicos_Colombia.csv", sep=",")
-
-datos = df.loc[100:110, "Código ICANH"]
-
-print(f"Datos: \n{datos}")
-
-
